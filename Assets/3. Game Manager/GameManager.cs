96d88397deefb185fca4ede62c5939687d1195e2
Alex <alex.steed88@gmail.com>
2021-01-18 16:09:42 +0900

    Game manager supports other systems.

---
+++ Assets/3. Game Manager/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : Singleton<GameManager>
+{
+	//TODO
+	//Load unload game levels
+	//keep track of game state
+	//generate other persistant system.
+	
+	public GameObject[] systemPrefabs;
+	List<GameObject> _systems;
+	
+	
+	#region level control
+	private string _currentScene = string.Empty;
+	private List<AsyncOperation> _loadOperationComplete;
+	private void OnLoadLevelComplete(AsyncOperation ao)
+	{
+		if(_loadOperationComplete.Contains(ao))
+		{
+			_loadOperationComplete.Remove(ao);
+		}
+		Debug.Log("Loadlevel Complete");
+	}
+	private void OnUnloadLevelComplete(AsyncOperation ao)
+	{
+		Debug.Log("Unloadlevel Complete");
+	}
+	
+	void LoadLevel(string levelName)
+	{
+		AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+		
+		if(ao == null)
+		{
+			Debug.LogError(this.name+": Could not load level "+levelName);
+			return;
+		}
+		ao.completed += OnLoadLevelComplete;
+	}
+	
+	void UnloadLevel(string levelName)
+	{
+		AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+		ao.completed += OnUnloadLevelComplete;
+		
+		if(ao == null)
+		{
+			Debug.LogError(this.name+": Could not load level "+levelName);
+			return;
+		}
+		
+	}
+	# endregion level control
+	
+	# region Systems
+	void InstantiateSystems()
+	{
+		GameObject system;
+		for (int i = 0; i < systemPrefabs.Length; i++) {
+			system = Instantiate(systemPrefabs[i]);
+			_systems.Add(system);
+		}
+		
+	}
+	
+	void DestroySystems()
+	{
+		foreach (var item in _systems)
+		{
+			Destroy(item);
+		}
+		_systems.Clear();
+	}
+	# endregion 
+	
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start()
+	{
+		DontDestroyOnLoad(gameObject);
+		
+		_systems  = new List<GameObject>();
+		_loadOperationComplete = new List<AsyncOperation>();
+		
+		InstantiateSystems();
+		
+		LoadLevel("Main");
+	}
+	
+	// This function is called when the MonoBehaviour will be destroyed.
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+		DestroySystems();
+	}
+}
---
+++ Assets/3. Game Manager/Utils/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+	public static T instance;
+	public bool isInitialized => instance == null;
+	
+	public static T Instance
+	{
+		get {return instance;}
+		
+	}
+	
+	
+	
+	protected void Awake()
+	{
+		if(instance != null)
+		{
+			Debug.Log("[Singleton] Trying to instatiant a second instance of singleton," + this);
+		}
+		else
+		{
+			instance = (T) this;
+		}
+	}
+
+	// This function is called when the MonoBehaviour will be destroyed.
+	protected virtual void OnDestroy()
+	{
+		if (instance == this)
+		{
+			instance = null;
+		}
+	}
+
+}
